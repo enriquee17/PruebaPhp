5852de7fe152ba433702d524305a35eb04758e57
enriquee17 <43639512+enriquee17@users.noreply.github.com>
2022-02-10 23:14:39 +0100

    CreacionServicios

--- prueba/config/app.php
+++ prueba/config/app.php
@@ -174,6 +174,9 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\SmtpProvider::class,
+        App\Providers\SesProvider::class,
+        App\Providers\NotificationServiceProvider::class,
 
     ],
 
